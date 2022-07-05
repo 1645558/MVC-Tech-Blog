2398990668d839f6256ebdbeae3bd155d79bee81
Rare Raptor <connorwtuncc@gmail.com>
2022-07-04 22:51:13 -0400

    added the login method

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -36,12 +36,39 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const userData = await User.Create({
+        const userData = await User.create({
             username: req.body.username,
             email: req.body.email,
             password: req.body.password,
         });
         res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findByPk({
+            where: {
+                email: req.body.email,
+            }
+        });
+
+        if (!userData) {
+            res.status(404).json({ message: 'No user with that email address!'})
+            return;
+        }
+
+        const password = userData.checkPassword(req.body.password);
+
+        if (!password) {
+            res.status(404).json({ message: 'Invalid password!'})
+            return;
+        }
+
+        res.json({ message: 'You are now logged in!' })
+
     } catch (err) {
         res.status(500).json(err);
     }
