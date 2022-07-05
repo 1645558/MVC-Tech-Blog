65308cd152fca2a377dfa9d6b7bfea8cac7f057a
Rare Raptor <connorwtuncc@gmail.com>
2022-07-04 21:44:37 -0400

    got it back to where i was before, everything is working as it should at the moment

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,3 @@
-const router = require('express').Router();
-const sequelize = require('../../config/connection');
-const { Post, User } = require('../../models');
+// const router = require('express').Router();
+// const sequelize = require('../../config/connection');
+// const { Post, User } = require('../../models');
