35ef9d9a07fe18aca5002d916ba6ee54391ca66c
Rare Raptor <connorwtuncc@gmail.com>
2022-07-04 22:42:48 -0400

    added more route methods to users api, everything working so far

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,50 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
 const { Post, User } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: ['password']}
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            attributes: { exclude: ['password']},
+            include: [
+                {
+                    model: Post,
+                    attributes: ['id', 'title', 'created_at']
+                }
+            ]
+        });
+
+        if (!userData) {
+            res.status(404).json({ message: 'No user matching this id!'})
+            return;
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.Create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
